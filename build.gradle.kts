e63b9b154e462be17eb5707311d7ae5295a5b713
Ioan Herbil <ionut.herbil@gmail.com>
2026-04-07 12:21:05 +0300

    chore: initialize Gradle project scaffold

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    application
+}
+
+group = "mediaPipeline"
+version = "1.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass = "mediaPipeline.PipelineRunner"
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+}
+
+repositories {
+    mavenCentral()
+}
